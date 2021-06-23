33a9cbd3e2150fc48468d6f92539880eb18320a0
goperzin <leandrolopes.help@gmail.com>
2021-06-23 01:22:27 -0300

    Primeiro commit

---
+++ consumer-cep/src/main/java/br/com/gpz/consumercep/ConsumerCepApplication.java
@@ -0,0 +1,13 @@
+package br.com.gpz.consumercep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerCepApplication.class, args);
+	}
+
+}
---
+++ consumer-cep/src/test/java/br/com/gpz/consumercep/ConsumerCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gpz.consumercep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
